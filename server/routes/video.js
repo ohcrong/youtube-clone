392802908f5ccb0fac2c80c25d6102e4b76dc6cd
kyuyoungoh <oqoq59@gmail.com>
2021-11-11 22:34:54 +0900

    video thumnail on router

--- server/routes/video.js
+++ server/routes/video.js
@@ -42,6 +42,34 @@ router.post('/uploadfiles', (req,res) => {
 
 router.post('/thumbnail', (req,res) => {
     //썸네일 생성, 비디오 러닝타임 가져오기
+    let thumbsFilePath ="";
+    let fileDuration ="";
+
+    ffmpeg.ffprobe(req.body.filePath, function(err, metadata){
+        console.dir(metadata);
+        console.log(metadata.format.duration);
+
+        fileDuration = metadata.format.duration;
+    })
+
+
+    ffmpeg(req.body.filePath)
+        .on('filenames', function (filenames) {
+            console.log('Will generate ' + filenames.join(', '))
+            thumbsFilePath = "uploads/thumbnails/" + filenames[0];
+        })
+        .on('end', function () {
+            console.log('Screenshots taken');
+            return res.json({ success: true, thumbsFilePath: thumbsFilePath, fileDuration: fileDuration})
+        })
+        .screenshots({
+            // Will take screens at 20%, 40%, 60% and 80% of the video
+            count: 3,
+            folder: 'uploads/thumbnails',
+            size:'320x240',
+            // %b input basename ( filename w/o extension )
+            filename:'thumbnail-%b.png'
+        });
 })
 
 module.exports = router;
